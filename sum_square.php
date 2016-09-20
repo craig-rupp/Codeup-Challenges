8efc3cdcd74e3a5cf2c293dd7ed894f65b0a2351
Craig Rupp <craigrupp@utexas.edu>
2016-09-20 09:30:32 -0500

    Sum square solution

--- sum_square.php
+++ sum_square.php
@@ -1,2 +1,25 @@
 <?php 
 
+//The sum of the squares of the first ten natural numbers is,
+//1^2 + 2^2 + ... + 10^2 = 385
+//The square of the sum of the first ten natural numbers is,
+//(1 + 2 + ... + 10)^2 = 552 = 3025
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+$sum1 = 0;
+$sum2 = 0;
+
+for ($i = 1; $i <= 100; $i++){
+	$sum1 += ($i * $i);
+	$sum2 += $i;
+}
+
+$sum2 *= $sum2;
+
+$difference = $sum2 - $sum1;
+
+echo "sum1 : " . $sum1 . PHP_EOL;
+echo "sum2 : " . $sum2 . PHP_EOL;
+echo $sum2 - $sum1 . PHP_EOL;
+echo "The difference is : " . ($sum2 - $sum1) . PHP_EOL;
